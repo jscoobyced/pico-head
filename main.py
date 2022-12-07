3ad1923e14a7ae71c697b2258287f1b6b5d45cc0
Cedric Rochefolle <no-name@githubuser.com>
2022-12-08 00:54:25 +0700

    Added SC08A controller code.

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
-from eyes import Eyes
+#from eyes import Eyes
+from sc08a import Sc08aController
+from time import sleep
+import uasyncio as asyncio
 
-e = Eyes(128, 64, 8, 9)
-e.blink(5)
-e.clear()
+#e = Eyes(128, 64, 8, 9)
+#e.blink(5)
+#e.clear()
+
+controller = Sc08aController(1)
+
+async def sender(h, s):
+    await h.enable_channel()
+    await h.set_angle(0, s, 1)
+    await h.set_angle(0, s, 2)
+    await h.set_angle(-90, s, 1)
+    await h.set_angle(0, s, 1)
+    await h.set_angle(90, s, 1)
+    await h.set_angle(0, s, 1)
+    await h.set_angle(-90, s, 2)
+    await h.set_angle(0, s, 2)
+    await h.set_angle(90, s, 2)
+    await h.set_angle(0, s, 2)
+    await h.close()
+
+async def main():
+    asyncio.create_task(sender(controller, 0))
+    while True:
+        await asyncio.sleep(1)
+
+def test():
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Interrupted')
+    finally:
+        asyncio.new_event_loop()
+
+test()
---
+++ sc08a.py
@@ -0,0 +1,64 @@
+import uasyncio as asyncio
+from machine import UART
+from time import sleep
+
+class Sc08aController:
+    _baudrate = 9600
+    
+    _MIN_ANGLE = 0
+    _MAX_ANGLE = 8000
+    
+    _writer_created = False
+    _reader_created = False
+
+    def __init__(self, uart_id):
+        self.uart = UART(uart_id, baudrate=self._baudrate)
+
+    async def close(self):
+        await self.disable_channel()
+        self.uart.deinit()
+
+    async def _write_byte(self, buffer):
+        if not self._writer_created:
+            self._writer_created = True
+            self._writer = asyncio.StreamWriter(self.uart, {})
+
+        self._writer.write(buffer)
+        await self._writer.drain()
+        await asyncio.sleep(0.5)
+
+    async def _read_bytes(self, len = 1):
+        if not self._reader_created:
+            self._reader_created = True
+            self._reader = asyncio.StreamReader(self.uart)
+        data = await self._reader.readline()
+        return data
+
+    async def init_controller(self, channel):
+        await self._write_byte(bytes([0x81, 0x0F, 0xA0, 0x0A]))
+        await self._write_byte(bytes([0x82, 0x0F, 0xA0, 0x0A]))
+        
+    async def enable_channel(self):
+        await self._write_byte(bytes([0xC0, 0x01, 0x0A]))
+        
+    async def disable_channel(self):
+        await self._write_byte(bytes([0xC0, 0x00, 0x0A]))
+
+    async def set_position_with_speed(self, position, speed, channel):
+        first_byte = 0b11100000 | channel
+        high_byte = (position >> 6) & 0b01111111
+        low_byte = position & 0b00111111
+        speed_byte = 0b00000000 | speed
+        await self._write_byte(bytes([first_byte, high_byte, low_byte, speed_byte, 0x0A]))
+
+    async def set_angle(self, angle, speed, channel):
+        if angle < -90 or angle > 90:
+            return False
+        
+        position = (angle + 90) * (self._MAX_ANGLE - self._MIN_ANGLE) / 180
+        position = int(position)
+        await self.set_position_with_speed(position, speed, channel)
+
+    async def get_position(self):
+        data = await self._read_bytes()
+        return data
